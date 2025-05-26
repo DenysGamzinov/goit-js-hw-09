665222a08ee165350ddce15c969b9347cb2b179a
DenysGamzinov <145193700+DenysGamzinov@users.noreply.github.com>
2025-05-26 15:51:39 +0300

    js0901

---
+++ src/js/2-form.js
@@ -0,0 +1,40 @@
+
+const formRef = document.querySelector(".feedback-form");
+const emailRef = formRef.elements['email'];
+const messageRef = formRef.elements['message'];
+
+formRef.addEventListener("input", onFormInput);
+
+function onFormInput() {
+    const userData = {
+        email: emailRef.value.trim(),
+        message: messageRef.value.trim(),
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(userData));
+}
+
+if (localStorage.length) {
+    const userDataStorage = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (userDataStorage.hasOwnProperty("email") && userDataStorage.hasOwnProperty("message")) {
+            emailRef.value = userDataStorage.email;
+            messageRef.value = userDataStorage.message;
+    }
+
+}
+
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    if (emailRef.value && messageRef.value) {
+    console.log({
+        email: emailRef.value.trim(),
+        message: messageRef.value.trim(),
+    });
+    localStorage.removeItem("feedback-form-state");
+    emailRef.value = "";
+    messageRef.value = "";
+    } else {
+        alert("All form fields must be filled in");
+    }
+}
